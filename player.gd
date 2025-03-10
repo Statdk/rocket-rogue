c4414cafb239ac60f2df30f6483d111671e84ca8
Statdk <me.statdk@gmail.com>
2025-03-10 03:13:37 -0700

    Refine controls to apply force

--- player.gd
+++ player.gd
@@ -1,39 +1,47 @@
 extends RigidBody2D
 
-var direction_mult = {
+var direction_mult = { # Define Directional Multipliers
 	"forward": 1.0,
-	"lateral": 0.5,
-	"backward": 0.7
+	"lateral": 0.7,
+	"backward": 0.5
 }
-var linear_moveSpeed = 200
-var angular_moveSpeed = 5
+var linear_thrust = 300 # Overall thrust multiplier
+var angular_thrust = 10000 # Overall yaw thrust multiplier
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var precalc_velocity = Vector2(0, 0)
+	# Initialize zero force vector
+	var precalc_force = Vector2(0, 0)
+	
+	# Calculate 0-1 force mult for each input
 	if Input.is_action_pressed("move_forward"):
-		precalc_velocity.y -= linear_moveSpeed * delta * direction_mult["forward"]
+		precalc_force.y -= direction_mult["forward"] * Input.get_action_strength("move_forward")
 	if Input.is_action_pressed("move_backward"):
-		precalc_velocity.y += linear_moveSpeed * delta * direction_mult["backward"]
+		precalc_force.y += direction_mult["backward"] * Input.get_action_strength("move_backward")
 	if Input.is_action_pressed("move_left"):
-		precalc_velocity.x -= linear_moveSpeed * delta * direction_mult["lateral"]
+		precalc_force.x -= direction_mult["lateral"] * Input.get_action_strength("move_left")
 	if Input.is_action_pressed("move_right"):
-		precalc_velocity.x += linear_moveSpeed * delta * direction_mult["lateral"]
+		precalc_force.x += direction_mult["lateral"] * Input.get_action_strength("move_right")
 	
+	# Apply overall mult
+	precalc_force *= linear_thrust
+	
+	# Rotate and apply force vector
+	constant_force.y = \
+		cos(rotation) * precalc_force.y + \
+		sin(rotation) * precalc_force.x
+	constant_force.x = \
+		cos(rotation) * precalc_force.x - \
+		sin(rotation) * precalc_force.y
+	
+	# Handle yaw forces
 	if Input.is_action_pressed("yaw_left"):
-		angular_velocity -= angular_moveSpeed * delta
-	if Input.is_action_pressed("yaw_right"):
-		angular_velocity += angular_moveSpeed * delta
-	
-	linear_velocity.y = linear_velocity.y + \
-		cos(rotation) * precalc_velocity.y + \
-		sin(rotation) * precalc_velocity.x
-	
-	linear_velocity.x = linear_velocity.x + \
-		cos(rotation) * precalc_velocity.x - \
-		sin(rotation) * precalc_velocity.y
+		constant_torque = -angular_thrust
+	elif Input.is_action_pressed("yaw_right"):
+		constant_torque = angular_thrust
+	else:
+		constant_torque = 0
